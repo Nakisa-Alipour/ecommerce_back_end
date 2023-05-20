1246b0c301d326328c5c496db82f941e01e3dca3
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-05-20 21:22:00 +1000

    modified handle errors

--- develop/routes/api/category-routes.js
+++ develop/routes/api/category-routes.js
@@ -1,79 +1,73 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
+// Error handling middleware
+const handleErrors = (res, err) => {
+  console.error(err);
+  res.status(500).json({ error: 'An unexpected error occurred' });
+};
 
+// Get all categories
 router.get('/', (req, res) => {
-  // find all categories
   Category.findAll({ include: [{ model: Product }] })
     .then((categoryData) => {
       res.json(categoryData);
     })
-    .catch((err) => {
-      // Handle error
-      console.log(err);
-      res.json(err);
-    });
+    .catch((err) => handleErrors(res, err));
 });
 
-
-
+// Get a single category by ID
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
   Category.findOne({
-    where: {id: req.params.id},
-    include: [{model: Product}]})
+    where: { id: req.params.id },
+    include: [{ model: Product }],
+  })
     .then((categoryData) => {
+      if (!categoryData) {
+        return res.status(404).json({ error: 'Category not found' });
+      }
       res.json(categoryData);
     })
-    .catch((err) => {
-      // Handle error
-      console.log(err);
-      res.json(err);
-    });
-
+    .catch((err) => handleErrors(res, err));
 });
 
+// Create a new category
 router.post('/', (req, res) => {
-  // create a new category
   Category.create({
-    category_name: req.body.category_name
-  }).then((categoryData) => {
-    res.json(categoryData);
+    category_name: req.body.category_name,
   })
-  .catch((err) => {
-    // Handle error
-    console.log(err);
-    res.json(err);
-  }); 
+    .then((categoryData) => {
+      res.status(201).json(categoryData);
+    })
+    .catch((err) => handleErrors(res, err));
 });
 
+// Update a category by ID
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   Category.update(req.body, {
-    where: {id: req.params.id}
-  }).then((categoryData) => {
-    res.json(categoryData);
+    where: { id: req.params.id },
   })
-  .catch((err) => {
-    // Handle error
-    console.log(err);
-    res.json(err);
-  });  
+    .then((categoryData) => {
+      if (categoryData[0] === 0) {
+        return res.status(404).json({ error: 'Category not found' });
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => handleErrors(res, err));
 });
 
+// Delete a category by ID
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-  Category.destroy(req.body, {
-    where: {id: req.params.id}
-  }).then((categoryData) => {
-    res.json(categoryData);
+  Category.destroy({
+    where: { id: req.params.id },
   })
-  .catch((err) => {
-    // Handle error
-    console.log(err);
-    res.json(err);
-  });  
+    .then((categoryData) => {
+      if (categoryData === 0) {
+        return res.status(404).json({ error: 'Category not found' });
+      }
+      res.status(204).end();
+    })
+    .catch((err) => handleErrors(res, err));
 });
 
 module.exports = router;
